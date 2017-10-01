04a3e00b9244e50e29b07edd602cd946c8c951f1
mrjkc <julius.kvedaras@coupa.com>
2017-10-01 20:44:49 +0100

    styling list and adding image

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({ key: API_KEY, term: 'testimony'}, (videos) => {
       this.setState({ videos });
     });
   }
